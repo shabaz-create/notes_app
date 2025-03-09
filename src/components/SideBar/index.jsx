76fbbb5c6e3bd2cb44d1acf2760c7a97634250ba
shabaz.create <shabaz.jnv@gmail.com>
2025-03-09 19:32:50 +0530

    navbar & sidebar done--final

--- src/components/SideBar/index.jsx
+++ src/components/SideBar/index.jsx
@@ -4,12 +4,12 @@ export const SideBar = ()=>{
 
     const getStyles=({isActive})=>{
         const styles='flex align-center gap-1 px-2 py-1 rounded-tr-full rounded-br-full';
-        
+
         return isActive? `text-slate-50 bg-indigo-800 ${styles}` : 
         ` hover:bg-indigo-800  hover:text-slate-50 ${styles}`
     }
     return(
-        <aside className="flex flex-col gap-5 border-r-2 border-gray-100 w-24 h-screen p-3 w-[150px]">
+        <aside className="flex flex-col gap-4 border-r-2 border-gray-100 w-24 h-screen p-3 w-[150px]">
             <NavLink to='/' className={getStyles}> 
                 <span class="material-symbols-outlined">
                  home
